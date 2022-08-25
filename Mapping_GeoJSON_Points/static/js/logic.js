ab26fa2fd7f93865843f1edd5561879de7490497
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-08-24 18:00:12 -0700

    modifying logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -55,5 +55,5 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
 //   .addTo(map);
 // });
 
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'graymap' tile layer to the map..
 streets.addTo(map);
